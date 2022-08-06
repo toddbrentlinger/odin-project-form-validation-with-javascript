f5379bd98035efba3a6f9849055a0acccae20007
toddbrentlinger <toddbrentlinger@gmail.com>
2022-08-05 20:05:07 -0700

    Continue form validation script
    Adds custom error handler to be passed to each FormInput object.
    Starts validation handlers for email and zipcode to pass more specific
    error messages.
    Allows zipcode input to accept optional 4 digits at the end of the
    general 5 digit zipcode.

--- src/index.js
+++ src/index.js
@@ -42,11 +42,19 @@ import './styles.scss';
 
         showError() {
             // Add each lised error if invalid
-            this.errorElement.textContent = Object.entries(this.errors) // Get [key, value] pairs of ERRORS in array
+            let outputText = Object.entries(this.errors) // Get [key, value] pairs of ERRORS in array
                 .filter(entry => this.inputElement.validity[entry[0]]) // Filter by invalid errors from input element
                 .map(entry => entry[1]) // Map to array of error messages to display
                 .join('\n'); // Join array of messages with newline separating them
 
+            if (this.customErrorHandler) {
+                const customErrorText = this.customErrorHandler(this.inputElement);
+                if (customErrorText) {
+                    outputText += `\n${customErrorText}`;
+                }
+            }
+
+            this.errorElement.textContent = outputText;
             this.errorElement.className = 'error active';
         }
     }
@@ -58,6 +66,107 @@ import './styles.scss';
             this.init();
         }
 
+        handleEmailValidation(inputElement) {
+            let outputMessagesArr = [];
+            let outputMessagesPerValidityState;
+
+            if (inputElement.validity.typeMismatch) {
+                outputMessagesPerValidityState = [];
+
+                // Valid character sequence: character followed by '@' symbol followed by another character
+                // x@y or x@y.z
+
+                // Check if '@' symbol is present
+                const bIncludesAtSymbol = inputElement.value.includes('@');
+                let bSingleAtSymbol;
+                if (bIncludesAtSymbol) {
+                    // Check if more than one '@' symbol (if reach here, guaranteed to have at least one '@' symbol)
+                    bSingleAtSymbol = inputElement.value.split('@').length === 2;
+                    if (bSingleAtSymbol) {
+                        // Check if value is just '@'
+                        if (inputElement.value.length === 1) {
+                            outputMessagesPerValidityState.push('Must include more than just an "@" symbol!');
+                        } else { // Else includes '@' symbol and at least one other character
+                            if (inputElement.value.startsWith('@')) {
+                                outputMessagesPerValidityState.push('Must NOT start with "@" symbol!');
+                            } else if (inputElement.value.endsWith('@')) {
+                                outputMessagesPerValidityState.push('Must NOT end with "@" symbol!');
+                            } else { // Else '@' symbol is sandwiched on either side by at least one character
+                                //console.error('Email should be valid if reach here!');
+                            }
+                        }
+                    } else { // Else multiple instances of '@' symbol
+                        outputMessagesPerValidityState.push('Must include only 1 "@" symbol!');
+                    }
+                } else { // Else '@' symbol is NOT present
+                    outputMessagesPerValidityState.push('Must include an "@" symbol!');
+                } 
+
+                // Check if '.' is present
+                if (inputElement.value.includes('.')) {
+                    // Check if valid characters before ('@' cannot be before '.')
+                    if (bSingleAtSymbol && /@./.test(inputElement.value)) {
+                        outputMessagesPerValidityState.push('Must not follow "@" symbol with "." symbol!');
+                    }
+                    // Check if valid characters after (any characters after are valid but must NOT end with '.' symbol)
+                    if (inputElement.value.endsWith('.')) {
+                        outputMessagesPerValidityState.push('Must NOT end with "." symbol!');
+                    }
+                }
+
+                // If NO custom error messages added, use generic message related to validity state
+                if (outputMessagesPerValidityState.length) {
+                    outputMessagesArr.push.apply(outputMessagesArr, outputMessagesPerValidityState);
+                } else {
+                    outputMessagesArr.push('Must be a valid email address type!');
+                }
+            }
+
+            return outputMessagesArr.join('\n');
+        }
+
+        handleZipCodeValidation(inputElement) {
+            let outputMessagesArr = [];
+            let outputMessagesPerValidityState;
+
+            if (inputElement.validity.patternMismatch) {
+                outputMessagesPerValidityState = [];
+
+                // Valid character sequence: 
+                // Regex: ^([0-9]{5})[-\s]?([0-9]{4}|)$
+                // 5 digits followed by optional space or hyphen followed by optional 4 digits
+
+                // Check if any non-valid characters are present (valid characters: 0-9, -, \s)
+                if (/[^0-9\s-]/.test(inputElement.value)) {
+                    outputMessagesPerValidityState.push('Must be numbered digits separated by optional space or hyphen!');
+                } else { // Else all characters are valid
+                    // Check for more than one separating character (dash or space)
+                    if (/[\s-][0-9]*[\s-]/.test(inputElement.value)) {
+                        outputMessagesPerValidityState.push('Must NOT have more than 1 separating character (dash or space)!');
+                    } else { // Else only one separating character (dash or space)
+                        // Get two separate sections of digits
+                        const digitMatch = inputElement.value.match(/([0-9]*)[\s-]?([0-9]*)/);
+                    }
+                }
+
+                if (outputMessagesPerValidityState.length) {
+                    outputMessagesArr.push.apply(outputMessagesArr, outputMessagesPerValidityState);
+                } else {
+                    outputMessagesArr.push('Must be 5 digits followed by optional space or hyphen followed by optional 4 digits!');
+                }
+            }
+
+            return outputMessagesArr.join('\n');
+        }
+
+        handlePasswordValidation(inputElement) {
+
+        }
+
+        handlePasswordConfirmationValidaiton(passwordConfirmInputElement, passwordInputElement) {
+
+        }
+
         init() {
             this.element.querySelectorAll('label').forEach(formLabel => {
                 let errors = {};
@@ -65,9 +174,10 @@ import './styles.scss';
                 switch (formLabel.querySelector('input').name) {
                     case 'email':
                         errors = {
-                            typeMismatch: 'Must be a valid email address!',
+                            //typeMismatch: 'Must be a valid email address!',
                             valueMissing: 'Email required!',
                         }
+                        customErrorHandler = this.handleEmailValidation.bind(this);
                         break;
                     case 'country':
                         errors = {
@@ -78,16 +188,17 @@ import './styles.scss';
                         break;
                     case 'zipcode':
                         errors = {
-                            patternMismatch: 'Must be a 5-digit number!',
+                            //patternMismatch: 'Must be 5 digits followed by optional space or hyphen followed by optional 4 digits!',
                             valueMissing: 'Zipcode required!',
                         }
+                        customErrorHandler = this.handleZipCodeValidation.bind(this);
                         break;
                     case 'password':
                     case 'password-confirm':
                         break;
                     default:
                 }
-                new FormInput(formLabel, errors);
+                new FormInput(formLabel, errors, customErrorHandler);
             });
         }
     }
